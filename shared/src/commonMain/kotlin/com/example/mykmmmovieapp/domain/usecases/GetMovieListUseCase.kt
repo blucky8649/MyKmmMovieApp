7d4b584b76411c8f15874043262de9b2c083d3b3
blucky8649 <blucky4511@gmail.com>
2022-10-02 19:50:24 +0900

    FEAT: UseCase 추가

---
+++ shared/src/commonMain/kotlin/com/example/mykmmmovieapp/domain/usecases/GetMovieListUseCase.kt
@@ -0,0 +1,17 @@
+package com.example.mykmmmovieapp.domain.usecases
+
+import com.example.mykmmmovieapp.domain.MovieRepository
+import com.example.mykmmmovieapp.domain.entity.MovieItem
+import kotlinx.coroutines.flow.Flow
+
+class GetMovieListUseCase(
+    private val movieRepository: MovieRepository
+) {
+    suspend operator fun invoke(refresh: Boolean, searchQuery: String): Flow<List<MovieItem>> {
+        if (refresh || searchQuery.isBlank()) {
+            // 만약 검색 쿼리가 빈값이면 그냥 이전 데이터 그대로 노출
+            return movieRepository.getMovieList(!refresh, searchQuery)
+        }
+        return movieRepository.getMovieList(refresh, searchQuery)
+    }
+}
